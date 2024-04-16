d5d6ead6619632f03ce4298ee3ec81d94bc0d672
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-04-16 15:26:54 +0200

    basic kegg download

--- workflow/scripts/construct/annotate_kegg.py
+++ workflow/scripts/construct/annotate_kegg.py
@@ -16,6 +16,8 @@ from rdflib import Graph, Namespace, URIRef, Literal, BNode
 import polars as pl
 from Bio.KEGG import Gene
 from Bio.KEGG import REST
+import time
+import re
 OVERWRITE = False
 
 
@@ -24,15 +26,19 @@ def read_metadata(filepath: str):
                       null_values="na")
 
 
-def collect_kegg_identifiers(metadata: pl.DataFrame):
+def collect_kegg_identifiers(metadata: pl.DataFrame, folder: str) -> set[str]:
    id_list = [metadata.get_column("KEGG1").to_list(),
               metadata.get_column("KEGG1").to_list()]
-   return set([i for i in id_list][0])
+   required_identifiers = set([i for i in id_list][0])
+   # found_identifiers = glob.glob(f"{folder}/*txt")
+   # downloaded = [re.findall(r"/(\w{3}\d{2}.+)\.txt",i)[0] for i in glob.glob("results/data/queries/sce/sce/*txt")]
+   return required_identifiers
 
 
-def download_kegg_information(kegg_genes: List[str], 
+def download_kegg_information(kegg_genes: List[str] | set[str], 
                              organism_code: str = "sce",
-                             folder = 'results/data/queries'):
+                             folder = 'results/data/queries',
+                             error_log = 'results/data/queries/'):
     """Function to download all the KEGG identifiers
 
     Parameters
@@ -45,9 +51,15 @@ def download_kegg_information(kegg_genes: List[str],
     for kegg_gene in kegg_genes:
         rq = f"http://rest.kegg.jp/get/{organism_code}:{kegg_gene}"
         result = requests.get(rq)
+        try:
+            result.raise_for_status()
+        except requests.HTTPError as  e:
+            print(f"{kegg_gene} could not be loaded\n {e}")
+
         outpath = Path(f"{folder}/{organism_code}/{kegg_gene}.txt")
         outpath.parent.mkdir(parents=True, exist_ok=True)
         write_text(result.text, output=outpath)
+        time.sleep(3) # avoid overburdening KEGG
 
 
 def write_text(text: str, output: Path, overwrite=False):
@@ -55,14 +67,14 @@ def write_text(text: str, output: Path, overwrite=False):
     output.write_text(text)
 
 
-
 def main():
    input_file = snakemake.input[0] 
+   output_folder = snakemake.output[0]
    metadata = read_metadata(input_file)
    kegg_identifiers = collect_kegg_identifiers(metadata=metadata)
-   print(len(kegg_identifiers))
-   download_kegg_information(["YLL041C"])
-
+   print(f"starting download of {len(kegg_identifiers)} identifiers")
+   download_kegg_information(kegg_identifiers,
+                            folder=output_folder)
 
 
 if __name__ == "__main__":
